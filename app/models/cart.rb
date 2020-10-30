c3a2e8fbb3c3f4f93b08aa6b3e0188c39a89a0c6
patrick204nqh <patrick204nqh@gmail.com>
2020-10-30 12:43:36 +0700

    fea empty cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,5 +9,9 @@ class Cart < ApplicationRecord
       current_item = line_items.build(product_id: product.id)
     end
     current_item
+
+  end
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
   end
 end
