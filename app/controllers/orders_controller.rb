e2f8948a848b9293a54ce634d3867fe5143621a2
patrick204nqh <patrick204nqh@gmail.com>
2020-10-30 16:16:58 +0700

    capturing the order details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,9 +28,13 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
 
     respond_to do |format|
       if @order.save
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to store_index_url, notice: 'Thank you for your order.'}
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
