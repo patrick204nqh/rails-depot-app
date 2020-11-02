a8d1ccc82d2b8dc072de55b14f9d401ee34ae2c1
patrick204nqh <patrick204nqh@gmail.com>
2020-11-02 10:40:30 +0700

    config mailer

---
+++ app/javascript/PayTypeSelector/CreditCardPayType.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class CreditCardPayType extends Component {
+  render() {
+    return (
+      <div>
+        <div className="field">
+          <label htmlFor="oroder_credit_card_number">CC #</label>
+          <input
+            type="password"
+            name="order[credit_card_number]"
+            id="order_credit_card_number"
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="order_expiration_date">Expiry</label>
+          <input
+            type="text"
+            name="order[expiration_date]"
+            id="order_expiration_date"
+            size="9"
+            placeholder="e.g. 03/19"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreditCardPayType;
---
+++ app/javascript/PayTypeSelector/PurchaseOrderPayType.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class PurchaseOrderPayType extends Component {
+  render() {
+    return (
+      <div>
+        <div className="field">
+          <label htmlFor="order_po_number">PO #</label>
+          <input type="password" name="order[po_number]" id="order_po_number" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PurchaseOrderPayType;
